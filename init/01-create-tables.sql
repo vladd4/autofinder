a45e996fa8b3fda227802e9a98623ade223ff4ed
Vladyslav D <donetsvlad444@gmail.com>
2024-12-18 22:45:09 +0200

    updated uuid table format

--- init/01-create-tables.sql
+++ init/01-create-tables.sql
@@ -1,6 +1,6 @@
 -- Clients Table
 CREATE TABLE clients (
-    id INT PRIMARY KEY,
+    id CHAR(36) PRIMARY KEY,  -- UUID format
     email VARCHAR(255),
     name VARCHAR(255),
     phone VARCHAR(15),
@@ -10,58 +10,58 @@ CREATE TABLE clients (
 
 -- Brands Table
 CREATE TABLE brands (
-    id INT PRIMARY KEY,
+    id CHAR(36) PRIMARY KEY,  -- UUID format
     brand VARCHAR(255)
 );
 
 -- Models Table
 CREATE TABLE models (
-    id INT PRIMARY KEY,
+    id CHAR(36) PRIMARY KEY,  -- UUID format
     model VARCHAR(255),
-    brand_id INT,
+    brand_id CHAR(36),
     FOREIGN KEY (brand_id) REFERENCES brands(id)
 );
 
 -- Sites Table
 CREATE TABLE sites (
-    id INT PRIMARY KEY,
+    id CHAR(36) PRIMARY KEY,  -- UUID format
     name VARCHAR(255),
     photo_url TEXT
 );
 
 -- Types Table
 CREATE TABLE types (
-    id INT PRIMARY KEY,
+    id CHAR(36) PRIMARY KEY,  -- UUID format
     type VARCHAR(255)
 );
 
 -- Fuels Table
 CREATE TABLE fuels (
-    id INT PRIMARY KEY,
+    id CHAR(36) PRIMARY KEY,  -- UUID format
     fuel VARCHAR(255)
 );
 
 -- Gearboxes Table
 CREATE TABLE gearboxes (
-    id INT PRIMARY KEY,
+    id CHAR(36) PRIMARY KEY,  -- UUID format
     gearbox VARCHAR(255)
 );
 
 -- Cars Table
 CREATE TABLE cars (
-    id CHAR(36) PRIMARY KEY,  -- Assuming UUID format for increment
+    id CHAR(36) PRIMARY KEY,  -- UUID format
     ad_name VARCHAR(255),
-    brand_id INT,
-    model_id INT,
+    brand_id CHAR(36),
+    model_id CHAR(36),
     year INT,
     price INT,
     mileage INT,
     photo_url TEXT,
-    type_id INT,
-    gearbox_id INT,
-    fuel_id INT,
+    type_id CHAR(36),
+    gearbox_id CHAR(36),
+    fuel_id CHAR(36),
     power INT,
-    site_id INT,
+    site_id CHAR(36),
     link TEXT,
     parsed_at TIMESTAMP,
     FOREIGN KEY (brand_id) REFERENCES brands(id),
@@ -74,21 +74,21 @@ CREATE TABLE cars (
 
 -- Saved Searches Table
 CREATE TABLE saved_searches (
-    id INT PRIMARY KEY,
-    client_id INT,
-    brand_id INT,
-    model_id INT,
+    id CHAR(36) PRIMARY KEY,  -- UUID format
+    client_id CHAR(36),
+    brand_id CHAR(36),
+    model_id CHAR(36),
     max_price FLOAT,
     min_price FLOAT,
     max_mileage INT,
     min_mileage INT,
     min_year INT,
     max_year INT,
-    type_id INT,
-    gearbox_id INT,
+    type_id CHAR(36),
+    gearbox_id CHAR(36),
     max_power FLOAT,
     min_power FLOAT,
-    fuel_id INT,
+    fuel_id CHAR(36),
     telegram BOOLEAN,
     FOREIGN KEY (client_id) REFERENCES clients(id),
     FOREIGN KEY (brand_id) REFERENCES brands(id),
